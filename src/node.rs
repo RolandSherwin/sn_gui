641be23898d234b15f084e0e098066608e47cb41
RolandSherwin <RolandSherwin@protonmail.com>
2022-12-27 16:33:37 +0530

    feat(node): display logs

--- src/node.rs
+++ src/node.rs
@@ -1,13 +1,19 @@
 use eframe::{
-    egui::{self, RichText},
+    egui::{self, RichText, ScrollArea},
     epaint::Color32,
 };
 use eyre::Result;
 use std::process::{Child, Command, Stdio};
-use tokio::sync::mpsc;
+use tokio::{
+    fs::File,
+    io::{AsyncBufReadExt, BufReader},
+    sync::mpsc,
+    task::JoinHandle,
+    time::Duration,
+};
 
 enum NodeState {
-    Running(Child),
+    Running,
     Idle,
 }
 
@@ -17,10 +23,20 @@ impl Default for NodeState {
     }
 }
 
+struct Log {
+    handle: JoinHandle<()>,
+    log_reciever: mpsc::Receiver<String>,
+    logs: Vec<String>,
+}
+
 #[derive(Default)]
 pub struct NodeRunner {
     pub current_network_name: Option<String>,
     node_state: NodeState,
+    // Cannot be inside node_state as we need mut ref to the things inside while still needing
+    // shared ref to self.
+    running_node_handle: Option<Child>,
+    log: Option<Log>,
     pub status_sender: Option<mpsc::Sender<RichText>>,
 }
 
@@ -31,10 +47,10 @@ impl NodeRunner {
                 egui::Layout::top_down_justified(egui::Align::Center),
                 |ui| {
                     let button_rect = egui::Rect::from_min_size(
-                        ui.min_rect().min + egui::vec2(-100.0, 80.0),
+                        ui.min_rect().min + egui::vec2(-100.0, 00.0),
                         egui::vec2(200.0, 100.0),
                     );
-                    match &mut self.node_state {
+                    match self.node_state {
                         NodeState::Idle => {
                             if ui
                                 .put(
@@ -45,11 +61,19 @@ impl NodeRunner {
                             {
                                 if let Some(network_name) = &self.current_network_name {
                                     match Self::run_node(network_name) {
-                                        Ok(handle) => {
+                                        Ok(command_handle) => {
+                                            let (log_handle, log_reciever) = self.read_log();
                                             self.send_status(RichText::new(
                                                 "Node is running!".to_string(),
                                             ));
-                                            self.node_state = NodeState::Running(handle)
+                                            let log = Log {
+                                                handle: log_handle,
+                                                logs: Vec::new(),
+                                                log_reciever,
+                                            };
+                                            self.running_node_handle = Some(command_handle);
+                                            self.log = Some(log);
+                                            self.node_state = NodeState::Running;
                                         }
                                         Err(err) => self.send_status(
                                             RichText::new(format!("Error: {err}"))
@@ -65,7 +89,7 @@ impl NodeRunner {
                             }
                         }
 
-                        NodeState::Running(handle) => {
+                        NodeState::Running => {
                             if ui
                                 .put(
                                     button_rect,
@@ -73,22 +97,67 @@ impl NodeRunner {
                                 )
                                 .clicked()
                             {
-                                if handle.kill().is_err() {
-                                    self.send_status(
-                                        RichText::new("Error: Failed to kill node")
-                                            .color(Color32::RED),
-                                    );
-                                };
-                                self.send_status(RichText::new(
-                                    "Node has been stopped!".to_string(),
-                                ));
+                                if let Some(log) = &mut self.log {
+                                    log.handle.abort();
+                                }
+                                if let Some(node_handle) = &mut self.running_node_handle {
+                                    if node_handle.kill().is_err() {
+                                        self.send_status(
+                                            RichText::new("Error: Failed to kill node")
+                                                .color(Color32::RED),
+                                        );
+                                    };
+                                    self.send_status(RichText::new(
+                                        "Node has been stopped!".to_string(),
+                                    ));
+                                }
                                 self.node_state = NodeState::Idle
                             }
+                        }
+                    }
+
+                    if let Some(log) = &mut self.log {
+                        ScrollArea::vertical()
+                            .max_height(400.0)
+                            .max_width(800.0)
+                            .auto_shrink([false; 2])
+                            .show(ui, |ui| {
+                                ui.vertical(|ui| {
+                                    for line in &log.logs {
+                                        ui.label(line);
+                                    }
+                                });
+                            });
+                        if let Ok(line) = log.log_reciever.try_recv() {
+                            log.logs.push(line);
                         }
                     }
                 },
             );
         });
+    }
+
+    fn read_log(&self) -> (JoinHandle<()>, mpsc::Receiver<String>) {
+        let (log_sender, log_reciever) = mpsc::channel(10000);
+        let handle = tokio::spawn(async move {
+            // allow time for the log file to be created
+            tokio::time::sleep(Duration::from_secs(2)).await;
+            let file = File::open("/home/roland/.safe/node/local-node/sn_node.log")
+                .await
+                .unwrap();
+            let mut lines = BufReader::new(file).lines();
+
+            loop {
+                // let mut contents = String::new();
+                while let Some(line) = lines.next_line().await.unwrap() {
+                    if log_sender.send(line).await.is_err() {
+                        log::error!("Failed to send logs");
+                    }
+                }
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+        });
+        (handle, log_reciever)
     }
 
     // send status to the footer
@@ -109,6 +178,7 @@ impl NodeRunner {
         // Now the child process exists with a panic, "failed printing to stdout"
         let handle = Command::new("safe")
             .args(args_run_node)
+            .env("RUST_LOG", "sn_node,sn_interface=trace")
             .stdout(Stdio::piped())
             .spawn()?;
         Ok(handle)
